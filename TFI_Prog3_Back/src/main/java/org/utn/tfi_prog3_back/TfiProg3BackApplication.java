54ec3766c72da90348a70d62ee270ff517bdd1e9
Emanuel Gómez Juárez <emagomezj@gmail.com>
2026-06-17 20:52:41 -0300

    Feat: Agregar modulo Backend completo al repositorio

---
+++ TFI_Prog3_Back/src/main/java/org/utn/tfi_prog3_back/TfiProg3BackApplication.java
@@ -0,0 +1,13 @@
+package org.utn.tfi_prog3_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfiProg3BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfiProg3BackApplication.class, args);
+    }
+
+}
---
+++ TFI_Prog3_Back/src/test/java/org/utn/tfi_prog3_back/TfiProg3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utn.tfi_prog3_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfiProg3BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
